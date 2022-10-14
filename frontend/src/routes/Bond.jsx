6eb110aa771fd25f75701c1d674e4d979f0dc956
Jesserc <raymondjesse713@gmail.com>
2022-10-14 23:45:22 +0100

    added input fields in bond component

--- frontend/src/routes/Bond.jsx
+++ frontend/src/routes/Bond.jsx
@@ -25,81 +25,90 @@ export default function Bond() {
   console.log(BondName, BondPrice, MarketPrice);
 
   return (
-    <Zoom delay={-500}>
-      <div className="bond">
-        <div>
-          {/* IoIosArrowRoundBack */}
-          <button className="close" onClick={() => navigate(-1)}>
-            {" "}
-            <IoIosArrowRoundBack />{" "}
-          </button>
-          <h2 className="bond__name">{BondName}</h2>
-        </div>
-        <div className="bond__details">
-          <div className="bond__detail">
-            <h3 className="bond_details bond_details--market">
-              Market Price
-            </h3>
-            <p bond_details bond_details--marketprice>
-              {BondPrice}
-            </p>
+    <div className="body">
+      <Zoom delay={-500}>
+        <div className="bond">
+          <div>
+            {/* IoIosArrowRoundBack */}
+            <button className="close" onClick={() => navigate(-1)}>
+              {" "}
+              <IoIosArrowRoundBack />{" "}
+            </button>
+            <h2 className="bond__name">{BondName}</h2>
           </div>
-          <div className="bond__detail">
-            <h3 className="bond_details bond_details--market">Bond Price</h3>
-            <p bond_details bond_details--marketprice>
-              {MarketPrice}
-            </p>
-          </div>
-        </div>
-        <div className="bonds">
-          <div className="bonds__give">
-            <div className="bonds_give bonds_give--image">
-              {/* images are suppose to be here */}
-              <img className="icons" src="../images/dai.png" alt=""/>
+          <div className="bond__details">
+            <div className="bond__detail">
+              <h3 className="bond_details bond_details--market">
+                Market Price
+              </h3>
+              <p bond_details bond_details--marketprice>
+                {BondPrice}
+              </p>
             </div>
-            <div className="bonds_give bonds_give--name">
-              <p className="bonds_give bonds_give--title">you give</p>
-            </div>
-          </div>
-          <div className="bonds__give">
-            <div className="bonds_give bonds_give--time">
-              {/* images are suppose to be here */}
-              <p className="bonds_give bonds_give--times">#</p>
-            </div>
-            <div className="bonds_give bonds_give--name">
-              <p className="bonds_give bonds_give--title">
-                vested for 7 days
+            <div className="bond__detail">
+              <h3 className="bond_details bond_details--market">Bond Price</h3>
+              <p bond_details bond_details--marketprice>
+                {MarketPrice}
               </p>
             </div>
           </div>
-          <div className="bonds__give">
-            <div className="bonds_give bonds_give--get">
-              {/* images are suppose to be here */}
-              <p className="bonds_give bonds_give--getimage">#</p>
-              <p className="bonds_give bonds_give--gets">5.8%</p>
+          <div className="bonds">
+            <div className="bonds__give">
+              <div className="bonds_give bonds_give--image">
+                {/* images are suppose to be here */}
+                <img className="icons" src="../images/dai.png" alt="" />
+              </div>
+              <div className="bonds_give bonds_give--name">
+                <p className="bonds_give bonds_give--title">you give</p>
+              </div>
             </div>
-            <div className="bonds_give bonds_give--name">
-              <p className="bonds_give bonds_give--title">you get</p>
+            <div className="bonds__give">
+              <div className="bonds_give bonds_give--time">
+                {/* images are suppose to be here */}
+                <p className="bonds_give bonds_give--times">#</p>
+              </div>
+              <div className="bonds_give bonds_give--name">
+                <p className="bonds_give bonds_give--title">
+                  vested for 7 days
+                </p>
+              </div>
+            </div>
+            <div className="bonds__give">
+              <div className="bonds_give bonds_give--get">
+                {/* images are suppose to be here */}
+                <p className="bonds_give bonds_give--getimage">#</p>
+                <p className="bonds_give bonds_give--gets">5.8%</p>
+              </div>
+              <div className="bonds_give bonds_give--name">
+                <p className="bonds_give bonds_give--title">you get</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="bond__bigbutton">
-          <p className="bond_bigbutton bond_bigbutton--text">name</p>
-        </div>
-        <Slide right delay={-500}>
-          <div className="checks">
-            {data &&
-              data.map((items, index) => {
-                return (
-                  <div key={index} className="checks__bond">
-                    <p className="checks_bonds">{items.bondname}</p>
-                    <p className="checks_bonds">{items.bondbalance}</p>
-                  </div>
-                );
-              })}
+          <div className="bond__bigbutton">
+            <p className="bond_bigbutton bond_bigbutton--text">name</p>
           </div>
-        </Slide>
-      </div>
-    </Zoom>
-  )
+          <Slide right delay={-500}>
+            <div className="checks">
+              {data &&
+                data.map((items, index) => {
+                  return (
+                    <div key={index} className="checks__bond">
+                      <p className="checks_bonds">{items.bondname}</p>
+                      <p className="checks_bonds">{items.bondbalance}</p>
+                    </div>
+                  );
+                })}
+            </div>
+          </Slide>
+
+          <div className="bond-form">
+            <form>
+              <input type="number" placeholder="enter token amount" />
+              <button>deposit</button>
+            </form>
+          </div>
+        </div>
+      </Zoom>
+    </div>
+  );
 }
