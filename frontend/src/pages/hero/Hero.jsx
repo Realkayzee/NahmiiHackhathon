ca00d6e23259cf5b5694b8466c45e9505154f92f
OdionOseiwe <oseiweifebhor@gmail.com>
2022-10-14 13:14:47 +0100

    initial commit

--- frontend/src/pages/hero/Hero.jsx
+++ frontend/src/pages/hero/Hero.jsx
@@ -6,9 +6,14 @@ import { Outlet, Link } from "react-router-dom";
 import "./hero.scss";
 <<<<<<< HEAD
 
+<<<<<<< HEAD
 =======
 import BondHeader from "./BondHeader";
 >>>>>>> 4694e0bb5878dc55463ab8b06923bfdb5a57f09a
+=======
+import BondHeader from "./BondHeader";
+
+>>>>>>> 3268cf4 (updated icons)
 const Hero = () => {
   return (
     <Fragment>
