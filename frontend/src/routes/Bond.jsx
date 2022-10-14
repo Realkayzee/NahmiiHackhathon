b580bfddceb2e4d5686ece571437d0de36e838c7
OdionOseiwe <oseiweifebhor@gmail.com>
2022-10-14 23:23:26 +0100

    updated icons

--- frontend/src/routes/Bond.jsx
+++ frontend/src/routes/Bond.jsx
@@ -10,14 +10,17 @@ export default function Bond() {
   const navigate = useNavigate();
   const id = useParams();
   const num = id.id;
+  // console.log(Data);
   let BondName;
   let BondPrice;
   let MarketPrice;
+  let BondImage;
   for (let i = 0; i < Data.length; i++) {
-    if (i === num) {
+    if (i == num) {
       BondName = Data[i].bondname;
       BondPrice = Data[i].bondprice;
       MarketPrice = Data[i].marketprice;
+      BondImage = Data[i].payoutAssetImage;
       break;
     }
   }
@@ -25,86 +28,81 @@ export default function Bond() {
   console.log(BondName, BondPrice, MarketPrice);
 
   return (
-    <div className="body">
-      <Zoom delay={-500}>
-        <div className="bond">
-          <div>
-            {/* IoIosArrowRoundBack */}
-            <button className="close" onClick={() => navigate(-1)}>
-              {" "}
-              <IoIosArrowRoundBack />{" "}
-            </button>
-            <h2 className="bond__name">{BondName}</h2>
-          </div>
-          <div className="bond__details">
-            <div className="bond__detail">
-              <h3 className="bond__details bond__details--market">
-                Market Price
-              </h3>
-              <p bond__details bond__details--marketprice>
-                {BondPrice}
-              </p>
-            </div>
-            <div className="bond__detail">
-              <h3 className="bond__details bond__details--market">
-                Bond Price
-              </h3>
-              <p bond__details bond__details--marketprice>
-                {MarketPrice}
-              </p>
-            </div>
-          </div>
-          <div className="bonds">
-            <div className="bonds__give">
-              <div className="bonds__give bonds__give--image">
-                {/* images are suppose to be here */}
-                <p className="bonds__give bonds__give--images">#</p>
-                <p className="bonds__give bonds__give--images">#</p>
-              </div>
-              <div className="bonds__give bonds__give--name">
-                <p className="bonds__give bonds__give--title">you give</p>
-              </div>
-            </div>
-            <div className="bonds__give">
-              <div className="bonds__give bonds__give--time">
-                {/* images are suppose to be here */}
-                <p className="bonds__give bonds__give--times">#</p>
-              </div>
-              <div className="bonds__give bonds__give--name">
-                <p className="bonds__give bonds__give--title">
-                  vested for 7 days
-                </p>
-              </div>
-            </div>
-            <div className="bonds__give">
-              <div className="bonds__give bonds__give--get">
-                {/* images are suppose to be here */}
-                <p className="bonds__give bonds__give--getimage">#</p>
-                <p className="bonds__give bonds__give--gets">5.8%</p>
-              </div>
-              <div className="bonds__give bonds__give--name">
-                <p className="bonds__give bonds__give--title">you get</p>
-              </div>
-            </div>
-          </div>
-          <div className="bond__bigbutton">
-            <p className="bond__bigbutton bond__bigbutton--text">name</p>
-          </div>
-          <Slide right delay={-500}>
-            <div className="checks">
-              {data &&
-                data.map((items, index) => {
-                  return (
-                    <div key={index} className="checks__bond">
-                      <p className="checks_bonds">{items.bondname}</p>
-                      <p className="checks_bonds">{items.bondbalance}</p>
-                    </div>
-                  );
-                })}
-            </div>
-          </Slide>
+    <Zoom delay={-500}>
+      <div className="bond">
+        <div>
+          {/* IoIosArrowRoundBack */}
+          <button className="close" onClick={() => navigate(-1)}>
+            {" "}
+            <IoIosArrowRoundBack />{" "}
+          </button>
+          <h2 className="bond__name">{BondName}</h2>
         </div>
-      </Zoom>
-    </div>
+        <div className="bond__details">
+          <div className="bond__detail">
+            <h3 className="bond__details bond__details--market">
+              Market Price
+            </h3>
+            <p bond__details bond__details--marketprice>
+              {BondPrice}
+            </p>
+          </div>
+          <div className="bond__detail">
+            <h3 className="bond__details bond__details--market">Bond Price</h3>
+            <p bond__details bond__details--marketprice>
+              {MarketPrice}
+            </p>
+          </div>
+        </div>
+        <div className="bonds">
+          <div className="bonds__give">
+            <div className="bonds__give bonds__give--image">
+              {/* images are suppose to be here */}
+              <img className="icon" src={`../${BondImage}`} alt=""/>
+            </div>
+            <div className="bonds__give bonds__give--name">
+              <p className="bonds__give bonds__give--title">you give</p>
+            </div>
+          </div>
+          <div className="bonds__give">
+            <div className="bonds__give bonds__give--time">
+              {/* images are suppose to be here */}
+              <p className="bonds__give bonds__give--times">#</p>
+            </div>
+            <div className="bonds__give bonds__give--name">
+              <p className="bonds__give bonds__give--title">
+                vested for 7 days
+              </p>
+            </div>
+          </div>
+          <div className="bonds__give">
+            <div className="bonds__give bonds__give--get">
+              {/* images are suppose to be here */}
+              <p className="bonds__give bonds__give--getimage">#</p>
+              <p className="bonds__give bonds__give--gets">5.8%</p>
+            </div>
+            <div className="bonds__give bonds__give--name">
+              <p className="bonds__give bonds__give--title">you get</p>
+            </div>
+          </div>
+        </div>
+        <div className="bond__bigbutton">
+          <p className="bond__bigbutton bond__bigbutton--text">name</p>
+        </div>
+        <Slide right delay={-500}>
+          <div className="checks">
+            {data &&
+              data.map((items, index) => {
+                return (
+                  <div key={index} className="checks__bond">
+                    <p className="checks_bonds">{items.bondname}</p>
+                    <p className="checks_bonds">{items.bondbalance}</p>
+                  </div>
+                );
+              })}
+          </div>
+        </Slide>
+      </div>
+    </Zoom>
   );
 }
--- frontend/src/utils/terms.js
+++ frontend/src/utils/terms.js
@@ -1,105 +1,58 @@
-// import { useParams } from 'react-router-dom';
-
-// export default function Purpose() {
-//   const [copy, setCopy] = useState(false);
-//   const [stable, setStable] = useState(true);
-//   const [route, setRoute] = useState("ETH");
-//   const ETH =
-//     "Saving in ETH can be volatile, the price will often change depending on the market, are you aware of the risk involved with saving in volatile token, we advice you save in USDT";
-//   const stablecoin =
-//     "You can now save in Stable coins. Please note that you can only withdraw it after the lock period you set, withdrawing before then attracts a penalty";
-//   const { isOpen, onOpen, onClose } = useDisclosure();
-
-//   const [isChecked, setIsChecked] = useState(false);
-
-//   const handleChange = () => {
-//     setIsChecked(!isChecked);
-//   };
-
-//   const { id } = useParams();
-
-//   const { address } = useAccount();
-
-//   const { data: getUserBanks } = usePiggyFactoryUserBanks(
-//     "getUserBanks",
-//     address
-//   );
-
-//   let init_tx_data = [];
-//   let purposeContractAddress = "";
-
-//   for (let i = 0; i < getUserBanks.length; i++) {
-//     if (id == i) {
-//       purposeContractAddress = getUserBanks[i];
-
-//       init_tx_data.push(
-//         {
-//           addressOrName: getUserBanks[i],
-//           contractInterface: PIGGY_BANK_ABI.abi,
-//           functionName: "getContractBalance",
-//         },
-//         {
-//           addressOrName: getUserBanks[i],
-//           contractInterface: PIGGY_BANK_ABI.abi,
-//           functionName: "getTokenBalance",
-//           args: [USDT_ADDRESS],
-//         },
-//         {
-//           addressOrName: getUserBanks[i],
-//           contractInterface: PIGGY_BANK_ABI.abi,
-//           functionName: "getTokenBalance",
-//           args: [USDC_ADDRESS],
-//         }
-//       );
-//     }
-//   }
-
 const Data = [
-    {
-      bondname: "NV-DOLA SLP",
-      marketprice: "$60",
-      bondprice: "$59",
-    },
-    {
-      bondname: "XRUNE-ETH SLP",
-      marketprice: "$600",
-      bondprice: "$58",
-    },
-    {
-      bondname: "USDT",
-      marketprice: "$6",
-      bondprice: "$9",
-    },
-    {
-      bondname: "USDC",
-      marketprice: "$6",
-      bondprice: "99",
-    },
-    {
-      bondname: "WETH",
-      marketprice: "$99",
-      bondprice: "79",
-    },
-    {
-      bondname: "polygon",
-      marketprice: "$90",
-      bondprice: "70",
-    },
-    {
-      bondname: "georli",
-      marketprice: "$66",
-      bondprice: "79",
-    },
-    {
-      bondname: "etherum classic",
-      marketprice: "$60",
-      bondprice: "77",
-    },
-    {
-      bondname: "bitcoin classic",
-      marketprice: "$60",
-      bondprice: "70",
-    },
-  ];
+  {
+    bondname: "NV-DOLA SLP",
+    marketprice: "$60",
+    bondprice: "$59",
+    payoutAssetImage: "images/eth.png",
+  },
+  {
+    bondname: "XRUNE-ETH SLP",
+    marketprice: "$600",
+    bondprice: "$58",
+    payoutAssetImage: "images/dai.png",
+  },
+  {
+    bondname: "USDT",
+    marketprice: "$6",
+    bondprice: "$9",
+    payoutAssetImage: "images/dola.png",
+  },
+  {
+    bondname: "USDC",
+    marketprice: "$6",
+    bondprice: "99",
+    payoutAssetImage: "images/usdc.png",
+  },
+  {
+    bondname: "WETH",
+    marketprice: "$99",
+    bondprice: "79",
+    payoutAssetImage: "images/eth.png",
+  },
+  {
+    bondname: "polygon",
+    marketprice: "$90",
+    bondprice: "70",
+    payoutAssetImage: "images/inv.png",
+  },
+  {
+    bondname: "georli",
+    marketprice: "$66",
+    bondprice: "79",
+    payoutAssetImage: "images/iq.png",
+  },
+  {
+    bondname: "etherum classic",
+    marketprice: "$60",
+    bondprice: "77",
+    payoutAssetImage: "images/fox.png",
+  },
+  {
+    bondname: "bitcoin classic",
+    marketprice: "$60",
+    bondprice: "70",
+    payoutAssetImage: "images/dai.png",
+  },
+];
 
-  export default Data;
+export default Data;
